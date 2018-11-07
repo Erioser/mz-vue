33edf58751b0c577ccb5fe664df4a8545d622ec0
Erioser <983357618@qq.com>
2018-11-07 14:04:24 +0800

    exit

--- src/pages/user-info/AppUserInfo.vue
+++ src/pages/user-info/AppUserInfo.vue
@@ -1,12 +1,19 @@
 <template>
     <section class="app-user-info">
       app-user-info
+      <button @click = "exit">exit</button>
     </section>
 </template>
 
 <script>
 export default {
-    
+    methods: {
+        exit () {
+            // 退出的时候去掉本地存储中存储的登录状态
+            localStorage.removeItem('userInfo')
+            this.$router.push({ name: 'home' })
+        }
+    }
 }
 </script>
 
